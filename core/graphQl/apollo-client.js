258c3d122190fa9fef8a565c6be1265c514d3764
desyed <syedshihabdu@gmail.com>
2022-02-06 06:52:57 +0600

    env

--- core/graphQl/apollo-client.js
+++ core/graphQl/apollo-client.js
@@ -4,7 +4,7 @@ const client = new ApolloClient({
     uri: "https://api.apito.io/secured/graphql",
     cache: new InMemoryCache(),
     headers: {
-        Authorization: 'Bearer 6rZvWY1K4jgvV6WNVR93c7RXxXeI6CwetOZv23K6sGbu3VhRIAEA2Zb3wlsDCqIaMAjkxyKA9ABNYIKKwl2HgxvQYSxtQqkGVlW0VuR4ugGHWx0LFQvCotao0jfaFIg8T0oBcyCvylRs5NRcAY5L3ho5wQObODx'
+        Authorization: process.env.TOKEN
     }
 });
 
